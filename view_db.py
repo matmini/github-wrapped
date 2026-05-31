77002630afec1a091245a6fa3504d875c24e69cd
Jazmin Iloso <matminisace15@gmail.com>
2026-06-01 01:00:18 +0800

    feat: add view commit table to view_db

--- view_db.py
+++ view_db.py
@@ -35,5 +35,48 @@ def view_repositories():
   cursor.close()
   conn.close()
 
+def view_commits():
+  """Queries the PostgreSQL and displays the commits"""
+  
+  # Connect using psycopg 
+  conn = psycopg.connect(
+    host = os.getenv("DB_HOST"), 
+    port=os.getenv("DB_PORT"),
+    dbname=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USER"), 
+    password=os.getenv("DB_PASSWORD")
+  )
+
+  cursor = conn.cursor() 
+
+    # Query to select all entries from out table 
+  cursor.execute("SELECT sha, repo_name , message, author_date FROM commits ORDER BY author_date DESC LIMIT 20;")
+  rows = cursor.fetchall() 
+
+  print("\n --- DATABASE ENTRIES (Sorted by Date) --- ")
+  print(f"{'sha':<40} | {'Repository Name':<20} | {'Message':<100} | {'Date':} ")
+  print("-"* 65) 
+
+  for row in rows:
+    short_sha = row[0][:10] # first ten chars 
+    repo = row[1]
+    msg = row[2].replace("\n", " ") #truncate long commit messages
+    short_msg = msg[:30] + "..." if len(msg) > 30 else msg
+    raw_date = row[3]
+    if raw_date:
+      # if it's already a datetime obj, format it. if it's a string, show the first 10 char
+      date_str = row[3].strftime("%Y-%m-%d") if hasattr(raw_date, "strftime") else str(raw_date)[:10]
+    else: 
+      date_str = "N/A"
+    
+    print(f"{short_sha:<12} | {repo:<20} | {date_str:<12} | {short_msg:<30}")
+  
+  cursor.execute("SELECT COUNT(*) FROM commits;")
+  total_commits = cursor.fetchone()[0]
+  print(f"Total Commits Stored: {total_commits}\n")
+
+  cursor.close()
+  conn.close()
 if __name__ == "__main__":
-  view_repositories()
+  # view_repositories()
+  view_commits()
